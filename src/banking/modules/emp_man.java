8f53a5c14fd5c25361e0f4d14f882c49cb0798c6
Benson Mburu <nguruben@gmail.com>
2015-09-30 17:55:28 +0300

    working jtable search filter

--- src/banking/modules/emp_man.java
+++ src/banking/modules/emp_man.java
@@ -33,7 +33,7 @@ Connection conn  = null;
        String sql = "select * from employees" ;
        pst=conn.prepareStatement(sql);
        rs=pst.executeQuery();
-       Table_render.setModel(DbUtils.resultSetToTableModel(rs));
+       table_disp.setModel(DbUtils.resultSetToTableModel(rs));
        
         }
         catch(Exception e){
@@ -58,6 +58,8 @@ Connection conn  = null;
         jLabel1 = new javax.swing.JLabel();
         txtSearchString = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table_disp = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -70,6 +72,19 @@ Connection conn  = null;
             }
         });
 
+        table_disp.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(table_disp);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -81,10 +96,13 @@ Connection conn  = null;
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(38, 38, 38)
-                        .addComponent(txtSearchString, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(60, 60, 60)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(65, Short.MAX_VALUE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 586, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(txtSearchString, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(60, 60, 60)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(95, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -94,7 +112,8 @@ Connection conn  = null;
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtSearchString, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
-                .addGap(0, 250, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -120,7 +139,7 @@ Connection conn  = null;
                     (com.mysql.jdbc.Statement)
                     conn.prepareStatement(sql);
             rs = (ResultSet) pst.executeQuery(sql);
-            Table_render.setModel(DbUtils.resultSetToTableModel(rs));
+            table_disp.setModel(DbUtils.resultSetToTableModel(rs));
             if(!rs.absolute(1)){ JOptionPane.showMessageDialog(null, "No results were found: " );}
                 
 
@@ -129,7 +148,7 @@ Connection conn  = null;
            
 
         }// TODO add your handling code here:
-    }                                        
+                                           
 
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -172,6 +191,8 @@ Connection conn  = null;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable table_disp;
     private javax.swing.JTextField txtSearchString;
     // End of variables declaration//GEN-END:variables
 }
