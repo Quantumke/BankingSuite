c4b2000ad9728c98437e664d0743764211a95584
Benson Mburu <nguruben@gmail.com>
2015-09-30 07:55:29 +0300

    initialized new jframe

--- src/banking/modules/hr.java
+++ src/banking/modules/hr.java
@@ -139,6 +139,11 @@ public class hr extends javax.swing.JFrame {
         jMenu1.add(jMenuItem1);
 
         jMenuItem3.setText("Salary Comp");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem3);
 
         jMenuItem4.setText("Paygrades");
@@ -251,6 +256,12 @@ public class hr extends javax.swing.JFrame {
                 EMP_LIST j= new EMP_LIST();
                 j.setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        salary_comp k = new salary_comp();
+        k.setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
